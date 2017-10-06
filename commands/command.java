8a97ea928751c7ca949abbdd2d631e29e4a12e9e
Usohatchi <eamon@itofisher.com>
2017-10-06 11:25:49 -0700

    Overall command framework added

--- commands/command.java
+++ commands/command.java
@@ -1,1 +1,25 @@
-Literally Nothing
+//Overall structure of commands
+//Will be extended by indivisual objects
+public class Command {
+	//Setup
+	//Initialize variables here
+	void init() {
+
+	}
+
+	//Runs at start
+	//Runs once
+	void start() {
+
+	}
+
+	//Loops
+	void loop() {
+
+	}
+
+	//Stops motors
+	void stop(){
+
+	}
+}
