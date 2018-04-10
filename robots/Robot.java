632ed0cc5e73031218f0c1cfb1135f504e33f916
Usohatchi <eamon@itofisher.com>
2018-04-10 13:57:12 -0700

    Robot class added. For future comptitions, a new robot will extend the Robot class. All hardware will be mapped and stored in the hardware Hashmap using super.addHardware(Key, Object) (example in file), and Robot will be passed to each Segment, then to each Command. In Commands use Robot.getHardware() to get the HashMap, then from there access the hardware using the key.

---
+++ robots/Robot.java
@@ -0,0 +1,34 @@
+//Overall structure of segments
+//Will be extended by indivisual objects
+//All commands must have these functions and variables
+
+//imports
+
+package robots;
+
+//import Commands
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Robot{
+
+	//Hardware Hashmap, holds all the hardware mapped motors, servos and sensors. Accesed by Commands.
+	private Map<String, Object> hardware = new HashMap<String, Object>();
+
+	//Constructor
+	public Robot() {
+		//Set passed values to object values here
+		//Ex: super.setHardware("motorR", hardwareMap.get(DcMotor.class, "Right_Front")
+	}
+
+	public void setHardware(String key, Object value) {
+		hardware.put(key, value);
+	}
+
+	public Map getHardware() {
+		return hardware;
+	}
+}
